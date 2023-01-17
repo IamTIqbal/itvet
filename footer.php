668a91619614ce40bec8e77f41a411dadc2de5fd
Tamim Iqbal <108043975+IamTIqbal@users.noreply.github.com>
2023-01-17 18:48:27 +0600

    update

---
+++ footer.php
@@ -0,0 +1,9 @@
+<div class="container">
+<footer class="text-center text-lg-start bg-white text-muted">
+
+        <div class="text-center p-3" style="background-color: rgba(0, 0, 0, 0.2);">©2023 AITVET All Right Reserved. Design and Developed by <span class="style7"><a class="text-white" href="https://iit.akbor.org/" target="_blank">Akbor IIT</a></span></div>
+  </div>
+  <!-- Copyright -->
+</footer>
+</div>
+<!-- Footer -->
---
+++ includes/head.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Ahsanullah Institute of Technical &amp; Vocational Education &amp; Training</title>
+    <link href="style.css" rel="stylesheet">
+    <script src="script.js"></script>
+    <link rel="shortcut icon" href="img/logo.png" type="image/x-icon">
+</head>
+<body>
+<div class="container" style="margin-top: 20px;margin-bottom:20px;">
+<!-- Menu -->
+<div class="row">
+  <div class="col-8">
+<a class="btn fs-3" href="index.php"><img src="img/logo.png" alt="Ahasanullah ITVET" width="30" class="d-inline-block align-text-top"><span>AITVET</a>
+    <a class="btn fs-5 btn-light" aria-current="page" href="index.php">Home</a>
+    <a class="btn fs-5 btn-light" aria-current="page" href="about-us.php">About us</a>
+   <a class="btn fs-5 btn-light" aria-current="page" href="admission.php">Admission</a>
+    <a class="btn fs-5 btn-light" aria-current="page" href="update.php">Update Info</a>
+  <a class="btn fs-5 btn-light" aria-current="page" href="student.php">Student Info</a>
+  </div>
+  <div class="col-4">
+      <form class="d-flex" action="student.php" method="GET">
+        <input class="btn form-control-sm btn-outline-secondary" type="search" name="id" placeholder="Admission ID" aria-label="Find">
+        <button class="btn btn-success" type="submit">Find</button>
+      </form> 
+      </div>
+  </div>
+</div>
